0d5c894f27a57e6cb69ec8c514e67aa27d88d46e
LeyaUnm <emeline.unmar@efrei.net>
2025-04-26 18:10:06 +0200

    completé bmp8b

--- bmp8.h
+++ bmp8.h
@@ -1,9 +1,24 @@
  typedef struct {
- unsigned char header[54];
- unsigned char colorTable[1024];
- unsigned char * data;
- unsigned int width;
- unsigned int height;
- unsigned int colorDepth;
- unsigned int dataSize;
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
  } t_bmp8;
+
+ /*Reading and Writing an 8-bit Grayscale Image*/
+
+ t_bmp8 * bmp8_loadImage(const char * filename);
+ void bmp8_saveImage(const char * filename, t_bmp8 * img);
+ void bmp8_free(t_bmp8 * img);
+ void bmp8_printInfo(t_bmp8 * img);
+
+/*Image Processing Functions*/
+
+ void bmp8_negative(t_bmp8 * img);
+ void bmp8_brightness(t_bmp8 * img, int value);
+ void bmp8_threshold(t_bmp8 * img, int threshold);
+
+ void bmp8_applyFilter(t_bmp8 * img, float ** kernel, int kernelSize);
