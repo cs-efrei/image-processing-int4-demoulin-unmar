fe945fe491e35ef98536cfffef2c5842bd253579
Hug0dml <151134534+Hug0dml@users.noreply.github.com>
2025-05-25 18:39:40 +0200

    Create data_types.h
    definition of the structure t_bmp8

---
+++ data_types.h
@@ -0,0 +1,14 @@
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
+
+typedef struct {
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
+} t_bmp8;
+
+#endif //DATA_TYPES_H
