0af90c19d19208ab9c7425bf3db42498f3a8fd68
LeyaUnm <emeline.unmar@efrei.net>
2025-05-25 16:02:26 +0200

    Update bmp24.c

--- bmp24.c
+++ bmp24.c
@@ -61,3 +61,15 @@ void bmp24_free(t_bmp24 *img) {
     bmp24_freeDataPixels(img->pixels, img->height);
     free(img);
 }
+
+ void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+     fseek(file, position, SEEK_SET);
+     fread(buffer, size, n, file);
+ }
+
+ void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+     fseek(file, position, SEEK_SET);
+     fwrite(buffer, size, n, file);
+ }
+
+
